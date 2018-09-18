d40ba76b70ede3c1960f98610c69a4dcd49d27f5
cwang-laptop <carlos.wang.uw@gmail.com>
2018-09-18 12:54:36 -0400

    added last part

--- misc_scripts/install_ue4.bash
+++ misc_scripts/install_ue4.bash
@@ -9,4 +9,9 @@ git clone -b 4.18 https://github.com/EpicGames/UnrealEngine.git
 cd UnrealEngine
 ./Setup.sh
 ./GenerateProjectFiles.sh 
+make
 
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "Now please refer to the remaining instructions to test UE4."
+echo "Find this here: https://wiki.unrealengine.com/Building_On_Linux"
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
